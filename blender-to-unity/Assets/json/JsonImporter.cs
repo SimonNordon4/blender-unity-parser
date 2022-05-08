50a755cc2cb7c13ea45ea5c15da2d6f1fef96528
SimonEnvisionVR <86404984+SimonNordon4@users.noreply.github.com>
2022-05-08 19:57:07 +1000

    Update Json Importer - Why is converting crap so difficulty.

--- blender-to-unity/Assets/json/JsonImporter.cs
+++ blender-to-unity/Assets/json/JsonImporter.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 using UnityEditor.AssetImporters;
 using System.IO;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using UnityEngine.UIElements;
 using Object = UnityEngine.Object;
 
 [ScriptedImporter(1,"ublend")]
@@ -18,15 +16,39 @@ public class JsonImporter : ScriptedImporter
 
         var blob = (File.ReadAllText(ctx.assetPath));
         var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(ctx.assetPath));
+        ImportMesh(blob);
+        
+    }
 
-        var name = (string)jsonObject["name"];
+    private void ImportMesh(string blob)
+    {
+        var meshJson = DataToDictionary(blob);
+        var name = meshJson["name"];
 
-        Debug.Log(jsonObject["name"]);
-        Debug.Log(jsonObject["vertices"]);
-        Debug.Log(jsonObject["normals"]);
-        Debug.Log(jsonObject["triangles"]);
+        var vertices = GetVector3FromData(meshJson["vertices"].ToString());
+    }
 
+    private Vector3[] GetVector3FromData(string vertices)
+    {
+        var vector3Datas = DataToDictionary(vertices);
 
+        for (int i = 0;i < vector3Datas.Count; i++)
+        {
+            var vector3Data = DataToArray<double>(vector3Datas[i.ToString()].ToString());
+            Debug.Log(vector3Data);
+        }
+        
+        return null;
+    }
+
+    public Dictionary<string, object> DataToDictionary(string data)
+    {
+        return JsonConvert.DeserializeObject<Dictionary<string, object>>(data);
+    }
+
+    public T[] DataToArray<T>(string data)
+    {
+        return JsonConvert.DeserializeObject<T[]>(data);
     }
 }
 
