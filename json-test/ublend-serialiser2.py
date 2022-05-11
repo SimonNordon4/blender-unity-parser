4cda8cdea8c2c5559ea74a99820403d358352196
SimonNordon4 <simon.nordon@protonmail.com>
2022-05-11 21:53:52 +1000

    Starting to break out python code to be more readable.

--- json-test/ublend-serialiser2.py
+++ json-test/ublend-serialiser2.py
@@ -5,36 +5,34 @@ import json
 import bpy
 
 # OBJECTIVE: SERIALIZE A MESH!
-
 # region Unity Classes
 
-
-class Vector2:
+class vector2:
+    ''' Unity Vector2 '''
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def toJson(self):
+    def tojson(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
-class Vector3:
+class vector3:
+    ''' Unity Vector3 '''
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
-    def toJson(self):
+    def tojson(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 # endregion
 
 # region Asset Classes
 
-# Represents a Unity Mesh.
-
-
-class UMesh:
+class unity_mesh:
+    ''' Unity Mesh Representation '''
     def __init__(self):
         name = ""
         vertices = []
@@ -49,92 +47,92 @@ class UMesh:
         uv7 = []
         uv8 = []
 
-    def toJson(self):
+    def tojson(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 # endregion
 
 # region Blender Classes
 
-
 projectExport = 'E:\\repos\\blender-to-unity\\json-test\\data_bpy.json'
 unityExport = 'E:\\repos\\blender-to-unity\\blender-to-unity\\Assets\\blender-to-unity\\data.ublend'
 
 # Calculate mesh data ver Loops
 
-
-class UMeshLoop2JSON:
-
+class mesh_to_unity_mesh:
+    # Serialize a Blender Mesh into a Unity Friendly JSON
     def get_uv(mesh):
         uv_maps = []
         for i in range(len(mesh.uv_layers)):
             uv_layer = []
             for j in range(len(mesh.uv_layers[i].data)):
-                uv = Vector2(mesh.uv_layers[i].data[j].uv.x, mesh.uv_layers[i].data[j].uv.y)
+                uv = vector2(mesh.uv_layers[i].data[j].uv.x, mesh.uv_layers[i].data[j].uv.y)
                 uv_layer.append(uv)
             uv_maps.append(uv_layer)
         return uv_maps
 
-    def Get(obj):
-        uMesh = UMesh()
-        uMesh.name = obj.name + "_loops"
-        uMesh.vertices = []
-        uMesh.normals = []
-        uMesh.triangles = []
-        uMesh.uv = []
+    def convert(obj):
+        u_mesh = unity_mesh()
+        u_mesh.name = obj.name + "_loops"
+        u_mesh.vertices = []
+        u_mesh.normals = []
+        u_mesh.triangles = []
+        u_mesh.uv = []
 
+        # TODO: apply modifiers and create virtual copy of the mesh.
+        # prep the mesh
         mesh = obj.data
-
-        # Triangulate.
         mesh.calc_loop_triangles()
         mesh.calc_normals_split()  # Split Normals are only accessible via loops (not verts)
 
         for loop in mesh.loops:
-            norm = Vector3(loop.normal.x, loop.normal.z, loop.normal.y)
+            norm = vector3(loop.normal.x, loop.normal.z, loop.normal.y)
             # now we have to do a tricky by getting vertices multiple times to match the split normals.
             v = (mesh.vertices[loop.vertex_index].co)
-            vert = Vector3(v.x, v.z, v.y)
-            uMesh.vertices.append(vert)
-            uMesh.normals.append(norm)
+            vert = vector3(v.x, v.z, v.y)
+            u_mesh.vertices.append(vert)
+            u_mesh.normals.append(norm)
 
         for tri in mesh.loop_triangles:
-            uMesh.triangles.append(tri.loops[0])
-            uMesh.triangles.append(tri.loops[2])
-            uMesh.triangles.append(tri.loops[1])
+            u_mesh.triangles.append(tri.loops[0])
+            u_mesh.triangles.append(tri.loops[2])
+            u_mesh.triangles.append(tri.loops[1])
 
         # Get UV Maps
         
-        uv_maps = UMeshLoop2JSON.get_uv(mesh)
-        uMesh.uv = uv_maps[0]
+        uv_maps = mesh_to_unity_mesh.get_uv(mesh)
+        if len(uv_maps) > 0:
+            u_mesh.uv = uv_maps[0]
         if len(uv_maps) > 1:
-            uMesh.uv2 = uv_maps[1]
+            u_mesh.uv2 = uv_maps[1]
         if len(uv_maps) > 2:
-            uMesh.uv3 = uv_maps[2]
+            u_mesh.uv3 = uv_maps[2]
         if len(uv_maps) > 3:
-            uMesh.uv4 = uv_maps[3]
+            u_mesh.uv4 = uv_maps[3]
         if len(uv_maps) > 4:
-            uMesh.uv5 = uv_maps[4]
+            u_mesh.uv5 = uv_maps[4]
         if len(uv_maps) > 5:
-            uMesh.uv6 = uv_maps[5]
+            u_mesh.uv6 = uv_maps[5]
         if len(uv_maps) > 6:
-            uMesh.uv7 = uv_maps[6]
+            u_mesh.uv7 = uv_maps[6]
         if len(uv_maps) > 7:
-            uMesh.uv8 = uv_maps[7]
+            u_mesh.uv8 = uv_maps[7]
 
-        return uMesh.toJson()
+        return u_mesh
 
 
-class WriteJson:
-    def Write(data, path):
+class ublend:
+    # manage ublend read and write (a.k.a JSON)
+    def write(data, path):
         with open(path, "w") as f:
-            f.write(data)
+            _json = data.tojson()
+            f.write(_json)
             f.close()
-
-
-class ReadJson:
-    def Read(path):
+    def read(path):
         data = open(projectExport, "r")
         return json.load(data)
+
+
 
 # endregion
 
@@ -142,10 +140,8 @@ class ReadJson:
 
 
 os.system('cls')
-print("starting export")
-data = UMeshLoop2JSON.Get(bpy.data.objects[0])
-print("json finished")
-WriteJson.Write(data, unityExport)
-result = ReadJson.Read(projectExport)
 
+data = mesh_to_unity_mesh.convert(bpy.data.objects[0])
+ublend.write(data, unityExport)
+result = ublend.read(projectExport)
 print(data)
