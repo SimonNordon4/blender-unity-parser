1962ce815f74dda7924f4e7c50f92d53d28a9255
SimonNordon4 <simon.nordon@protonmail.com>
2022-06-11 11:22:14 +1000

    more binary tests/

--- blender-to-unity/Assets/01-scripts/blend-parser/BlendBinaryTest.cs
+++ blender-to-unity/Assets/01-scripts/blend-parser/BlendBinaryTest.cs
@@ -7,6 +7,7 @@ using Kaitai;
 using System.Runtime.Serialization.Formatters.Binary;
 using System;
 using System.IO;
+using System.Text;
 
 
 //MESH: SdnaStruct[71] OBJECT: SdnaStruct[172]
@@ -18,29 +19,30 @@ public class BlendBinaryTest : MonoBehaviour
     {
         var blend = BlenderBlend.FromFile(m_blendFilePath);
 
+        var blocks = blend.Blocks;
+        var sdnas = blend.SdnaStructs;
 
-        // Find all mesh Blocks.
-        for (int i = 0; i < blend.Blocks.Count; i++)
+        // Block 1724 is a mesh.
+        var mesh_cube = blocks[1724];
+        var mesh_sdna = sdnas[(int)mesh_cube.SdnaIndex];
+
+        foreach (var field in mesh_sdna.Fields)
         {
-            if(blend.Blocks[i].SdnaIndex == 71)
-            {
-                var meshBlock = blend.Blocks[i];
-                print(meshBlock.Code);
-                print(i);
-                print(meshBlock.SdnaIndex);
-                foreach(var field in meshBlock.SdnaStruct.Fields)
-                {
-                    print($"\tfield.Name {field.Name} fieled.Type {field.Type}");
-                }
-                print(meshBlock.Body.ToString());
-
-
-                // https://wiki.blender.jp/Dev:Source/Architecture/File_Format
-                
-                print(meshBlock.M_RawBody);
-            }
+            Debug.Log(field.Name);
+            Debug.Log(field.Type);
         }
 
+        var mesh_bytes = (byte[])mesh_cube.Body;
+        
+        https://wiki.blender.jp/Dev:Source/Architecture/File_Format
+        using (BinaryReader reader = new BinaryReader(new MemoryStream(mesh_bytes)))
+        {
+            print(reader.BaseStream.Position);
+            var id = reader.ReadBytes((int)mesh_cube.LenBody);
+            print(Encoding.ASCII.GetString(id));
+            print(reader.BaseStream.Position);
+        }
+        print("");
     }
 }
 #endif
